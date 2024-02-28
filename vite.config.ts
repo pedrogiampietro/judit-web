4436f77de9756f1b83852c96317eed3ab71eaa43
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-02-28 15:37:38 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: mode === "production" ? "/assets/" : "/",
+  base: mode === "production" ? "/" : "/",
 }));
