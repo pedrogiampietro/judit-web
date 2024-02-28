f9e53813fccb576fc7edf4695703fce2933514f8
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-02-28 15:14:23 -0300

    fix: deploy

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -1,7 +1,7 @@
+import React, { useMemo } from "react";
 import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import { useDndContext, type UniqueIdentifier } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
-import { useMemo } from "react";
 import { TaskCard } from "./TaskCard";
 import { cva } from "class-variance-authority";
 import { Card, CardContent, CardHeader } from "./ui/card";
